2731f572dc89d9d31fdc00715462423dcbf668ea
MatheusCJesus <matheus.cerqueira100@gmail.com>
2024-04-06 23:21:44 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Create a nano GPT version from scratch',
+    author='Matheus Cerqueira de Jesus',
+    license='',
+)
